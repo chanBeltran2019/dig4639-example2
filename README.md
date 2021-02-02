35c679a3f7358973bdf35c39f40e19ef6f97a663
chanBeltran2019 <chantalbel2019@gmail.com>
2021-02-02 18:27:05 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dig4639-example2
+
+favorite quotation: "Yesterday, when it was tomorrow, it was too much day for me.” - Winnie the Pooh
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotations = [
+    " Yesterday, when it was tomorrow, it was too much day for me. - Winnie the Pooh",
+    "For to me, living means living for Christ, and dying is even better. - Paul the Apostle",
+    "A little consideration, a little thought for others, makes all the difference. - Winnie the Pooh"
+]
+let reiteration = (Math.floor(Math.random() * 3))
+console.log(quotations[reiteration])
